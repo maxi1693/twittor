9f28f43683bbfd8bbed2ac3d963a2e97c3aaa819
maxi1693 <maxi.1693.vm@gmail.com>
2019-01-04 01:57:21 -0300

    moificaciones para ios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/libs/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
